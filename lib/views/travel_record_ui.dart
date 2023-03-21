42e75c1bb15c449dc1d4f3cbaead60328d6fd1c1
s6319410018 <s6319410018@sau.ac.th>
2023-03-21 15:21:27 +0700

    v.2 by 6319410018

---
+++ lib/views/travel_record_ui.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:meta/meta_meta.dart';
+
+class Travel_Ui extends StatefulWidget {
+  const Travel_Ui({super.key});
+
+  @override
+  State<Travel_Ui> createState() => _Travel_UiState();
+}
+
+class _Travel_UiState extends State<Travel_Ui> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
