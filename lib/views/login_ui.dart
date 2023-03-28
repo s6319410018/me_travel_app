010510dedd0b1d4d82046f9b926331a78c1973de
s6319410018 <s6319410018@sau.ac.th>
2023-03-28 15:35:55 +0700

    v.1 by 6319410018

--- lib/views/login_ui.dart
+++ lib/views/login_ui.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home_Ui extends StatefulWidget {
   const Home_Ui({super.key});
@@ -12,6 +13,139 @@ class Home_Ui extends StatefulWidget {
 class _Home_UiState extends State<Home_Ui> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[300],
+        title: Center(
+          child: Text(
+            'บันทึกการเดินทาง',
+            style: GoogleFonts.kanit(),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.05,
+              ),
+              Image.asset(
+                'assets/images/logo.png',
+                width: MediaQuery.of(context).size.width * 0.3,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.08,
+              ),
+              Text(
+                'เข้าใช้งานแอปพลิเคชั่น',
+                style: GoogleFonts.kanit(
+                    fontSize: MediaQuery.of(context).size.width * 0.07),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.01,
+              ),
+              Text(
+                'บันทึกการเดินทาง',
+                style: GoogleFonts.kanit(
+                    fontSize: MediaQuery.of(context).size.width * 0.05),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.02,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * 0.15),
+                child: Row(
+                  children: [
+                    Text(
+                      'ชื่อผู้ใช้ : ',
+                      style: GoogleFonts.kanit(
+                          fontSize: MediaQuery.of(context).size.width * 0.05),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  right: MediaQuery.of(context).size.width * 0.15,
+                  left: MediaQuery.of(context).size.width * 0.15,
+                ),
+                child: TextField(),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.width * 0.06),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * 0.15),
+                child: Row(
+                  children: [
+                    Text(
+                      'รหัสผ่าน : ',
+                      style: GoogleFonts.kanit(
+                          fontSize: MediaQuery.of(context).size.width * 0.05),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  right: MediaQuery.of(context).size.width * 0.15,
+                  left: MediaQuery.of(context).size.width * 0.15,
+                ),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.visibility_off,
+                        color: Colors.green[200],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.02,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'SIGN IN',
+                  style: GoogleFonts.kanit(),
+                ),
+                style: ElevatedButton.styleFrom(
+                    fixedSize: Size(MediaQuery.of(context).size.width * 0.7,
+                        MediaQuery.of(context).size.width * 0.0125),
+                    backgroundColor: Colors.green[300]),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Checkbox(value: false, onChanged: (val) {}),
+                  Text(
+                    ' จำค่าการเข้าใช้งานแอปพลิเคชั่น ',
+                    style: GoogleFonts.kanit(
+                        fontSize: MediaQuery.of(context).size.width * 0.04),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.02,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'ลงทะเบียนเข้าใช้งาน ',
+                  style: GoogleFonts.kanit(
+                      fontSize: MediaQuery.of(context).size.width * 0.05),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      backgroundColor: Colors.green[50],
+    );
   }
 }
--- lib/views/splash_screen_ui.dart
+++ lib/views/splash_screen_ui.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:me_travel_app/views/login_ui.dart';
 
 class Splash_Ui extends StatefulWidget {
   const Splash_Ui({super.key});
@@ -11,11 +13,54 @@ class Splash_Ui extends StatefulWidget {
 
 class _Splash_UiState extends State<Splash_Ui> {
   @override
+  void initState() {
+    //ทำหน้านี้ให้เป็น SplashScreen
+    Future.delayed(
+        Duration(
+          seconds: 3,
+        ), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Home_Ui(),
+        ),
+      );
+    });
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green[300],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: MediaQuery.of(context).size.width * 0.5,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.width * 0.1,
+            ),
+            Text(
+              'บันทึกการเดินทาง',
+              style: GoogleFonts.kanit(
+                  fontSize: MediaQuery.of(context).size.width * 0.09,
+                  color: Colors.black),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.width * 0.1,
+            ),
+            Text(
+              'Created by : 6319410018\n Pattarapong Ekwongsa',
+              style: GoogleFonts.kanit(
+                  fontSize: MediaQuery.of(context).size.width * 0.05,
+                  color: Colors.black),
+            ),
+          ],
         ),
       ),
     );
